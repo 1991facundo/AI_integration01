0d5bde2cb596337c9d5b78c2d9e9064bd7e7fa17
Facundo Fernandez <99399998+1991facundo@users.noreply.github.com>
2024-07-15 14:35:23 +0200

    Project started

---
+++ ai_integration01/app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <h1>Hola Mundo</h1>
+    </div>
+  );
+};
+
+export default HomePage;
+  
